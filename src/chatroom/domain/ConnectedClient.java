40f00ad1e008c5dd5644b4df81319ca3c1041542
Yacine Bestaoui <yacine-fadl.bestaoui@etu.univ-nantes.fr>
2022-11-02 17:37:12 +0100

    refactor ConnectedClient

---
+++ src/chatroom/domain/ConnectedClient.java
@@ -0,0 +1,40 @@
+package chatroom.domain;
+
+import java.util.Objects;
+
+import src.ChatClientInterface;
+
+public class ConnectedClient {
+	public String name;
+	public ChatClientInterface client;
+
+	public ConnectedClient(String name, ChatClientInterface client) {
+		this.name = name;
+		this.client = client;
+	}
+	
+	public ConnectedClient(ConnectedClient c) {
+		this.name = c.getName();
+		this.client = c.getClient();
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public ChatClientInterface getClient() {
+		return client;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ConnectedClient other = (ConnectedClient) obj;
+		return Objects.equals(name, other.name);
+	}
+}
