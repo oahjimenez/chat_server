3bb017d6eaf506a7f8183e3b56b13ec31d63e71c
Oswaldo Jimenez <oswaldo-andres.jimenez-hidalgo@etu.univ-nantes.fr>
2022-11-27 16:27:34 +0100

    refactor host construction

--- src/chatroom/MainServer.java
+++ src/chatroom/MainServer.java
@@ -2,18 +2,22 @@ package chatroom;
 
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.util.logging.Logger;
 
 public class MainServer {
 
-	public static int COM_PORT = 1009;
+	public static final int COM_PORT = 1009;
+	public static final String HOST = "localhost";
+	public static final String GROUP_CHAT_SERVICE = "GroupChatService";
+
+	private static final Logger log = Logger.getLogger(MainServer.class.getName());
 
 	public static void main(String[] args) {
 		try {
 			LocateRegistry.createRegistry(COM_PORT);
-			new ChatServerGUI("rmi://localhost:" + COM_PORT + "/GroupChatService");
+			new ChatServerGUI(String.format("rmi://%s:%s/%s", HOST, COM_PORT, GROUP_CHAT_SERVICE));
 		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			log.severe(e.getMessage());
 		}
 	}
 
